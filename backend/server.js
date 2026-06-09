b9749b549e5949d6782d2e122de601d33342f899
Aditya <wakaleaditya234@gmail.com>
2026-06-09 16:19:29 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,10 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const path = require("path");
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const productRoutes = require("./routes/productRoutes");
 const orderRoutes = require("./routes/orderRoutes");
@@ -13,36 +15,10 @@ const notificationRoutes = require("./routes/notificationRoutes");
 const wishlistRoutes = require("./routes/wishlistRoutes");
 
 dotenv.config();
-console.log("ENV Cloud:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("ENV Mongo:", process.env.MONGO_URI);
+
 connectDB();
 
 const app = express();
-const path = require("path");
-app.use(cors());
-app.use(express.json());
-app.use(
-  "/api/auth",
-  authRoutes
-);
-app.use("/api/products", productRoutes);
-app.get("/", (req, res) => {
-  res.send("Farm Connect API Running");
-});
-
-
-const PORT = process.env.PORT || 5000;
-app.use(
-  "/uploads",
-  express.static(
-    path.join(__dirname, "uploads")
-  )
-);
-app.listen(PORT, () => {
-
-});
-
-
 
 app.use(
   cors({
@@ -52,12 +28,33 @@ app.use(
     ],
     credentials: true,
   })
+
 );
-app.use(
-  "/api/notifications",
-  notificationRoutes
-);
-app.use("/api/wishlist", wishlistRoutes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  "/uploads",
+  express.static(
+    path.join(__dirname, "uploads")
+  )
+);
+app.get("/", (req, res) => {
+  res.send("Farm Connect API Running");
+});
+
+app.use( "/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/notifications",notificationRoutes);
+app.use("/api/wishlist", wishlistRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+
+});
+
+
+
+
