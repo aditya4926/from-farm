78b4220d49ca1780bd9965c974303ecf4a7bb83f
Aditya <wakaleaditya234@gmail.com>
2026-06-14 13:37:21 +0530

    Add admin delete user

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -51,8 +51,30 @@ const getAllUsers = async (req, res) => {
     });
   }
 };
+const deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    await user.deleteOne();
+
+    res.json({
+      message: "User deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 
 module.exports = {
   getAdminStats,
   getAllUsers,
+  deleteUser,
 };
